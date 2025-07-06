081205c09d3b5331a3103f17b9c86c2f9fa377f5
prowl107 <milesosborne182@gmail.com>
2025-07-06 10:46:57 -0500

    Fixed no sync scripts

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import featured_projects from "./page_components/featured_projects";
 import contactForm from "./page_components/contactForm";
 import footer from "./page_components/footer";
 import timeline from "./page_components/timeline";
+import Script from 'next/script'
 
 export default async function Home() {
 
@@ -27,12 +28,12 @@ export default async function Home() {
       {footer()}
 
       {/* Scripts */}
-      <script src="js/jquery.min.js"></script>
-      <script src="js/jquery.poptrox.min.js"></script>
-      <script src="js/browser.min.js"></script>
-      <script src="js/breakpoints.min.js"></script>
-      <script src="js/util.js"></script>
-      <script src="js/main.js"></script>
+      <Script src="js/jquery.min.js"></Script>
+      <Script src="js/jquery.poptrox.min.js"></Script>
+      <Script src="js/browser.min.js"></Script>
+      <Script src="js/breakpoints.min.js"></Script>
+      <Script src="js/util.js"></Script>
+      <Script src="js/main.js"></Script>
     </div>
   );
 }
